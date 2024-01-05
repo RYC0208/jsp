a1c8c2755ccd5a86b9bb997f01335e205e3e6695
RYC0208 <ryc0208@naver.com>
2024-01-05 09:44:20 +0900

    adsssssss

--- ch11_ajax/src/main/java/ajaxPro1/AjaxController4.java
+++ ch11_ajax/src/main/java/ajaxPro1/AjaxController4.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import com.google.gson.Gson;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,17 +20,23 @@ public class AjaxController4 extends HttpServlet {
 		ArrayList<Member> alist = new MemberDao().getAllMember();
 		
 		// JSONArray [{key:value},{key:value},...]
+		/*
 		JSONArray jArr = new JSONArray();
 		for(Member m : alist) {
 			JSONObject jobj = new JSONObject();
 			jobj.put("id", m.getId());
 			jobj.put("name", m.getName());
-			jobj.put("gender", m.getGender());
-			jobj.put("email", m.getEmail());
+			jobj.put("gaender", m.getGender());
+			jobj.put("emil", m.getEmail());
 			jArr.add(jobj);
 		}
 		
 		response.setContentType("appliction/json; charset=UTF-8");
 		response.getWriter().print(jArr);
+		*/
+		
+		// GSON 사용시 2줄이면 됨
+		response.setContentType("application/json; charset=UTF-8");
+		new Gson().toJson(alist, response.getWriter());
 	}
 }
